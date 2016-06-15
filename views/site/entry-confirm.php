273812188dc1731e5779c9238b597e5657ed702d
zhoujj <zhoujj@jiedaibao.com>
2016-06-15 18:16:10 +0800

    delete

--- views/site/entry-confirm.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-
-<p> This your Information :</p>
-<ul>
-	<li><label>Name</label>:<?=Html::encode($model->name);?></li>
-	<li><label>Email</label>:<?=Html::encode($model->email);?></li>
-</ul>
