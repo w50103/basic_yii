fc18ff621f4114c29a5f0c916d572bf43262ca8a
zhoujj <zhoujj@jiedaibao.com>
2016-09-08 17:24:05 +0800

    re

--- config/local-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=test',
-    'username' => 'root',
-    'password' => '@dminCCTV19861214',
-    'charset' => 'utf8',
-];
